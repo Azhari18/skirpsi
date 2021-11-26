7e15931a37e3c260cc3a9fd7139a4f42dc19fc11
Azhari18 <azhari.18des@gmail.com>
2021-11-26 08:20:09 +0700

    first condition

--- belajar/database/seeders/DatabaseSeeder.php
+++ belajar/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,67 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(3)->create();
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::factory(20)->create();
+
+        // User::create([
+        //     'name' => 'azhari arsyad' ,
+        //     'email' =>'azhari.18des@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        // User::create([
+        //     'name' => 'arsyad azhari' ,
+        //     'email' =>'azhari.20des@gmail.com',
+        //     'password' => bcrypt('54321')
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum pertama',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quisquam facere possimus nam molestiae, debitis quo vel quae exercitationem sed tempore voluptatibus, culpa quibusdam fugiat dolor commodi odio autem quis minima expedita. Quam minima modi quisquam et similique rem incidunt, ducimus necessitatibus nesciunt quaerat, officia possimus sint odio commodi eaque assumenda laboriosam pariatur optio autem dolores rerum? Amet nobis non reprehenderit dolore omnis in inventore, officia nam esse quaerat aspernatur maiores alias tenetur commodi, sed voluptatem similique sit labore minus quasi delectus. Corrupti eligendi sequi eius vel consectetur, dolor repudiandae, atque, consequuntur explicabo minus quas doloribus tempore pariatur libero unde!',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum kedua',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quisquam facere possimus nam molestiae, debitis quo vel quae exercitationem sed tempore voluptatibus, culpa quibusdam fugiat dolor commodi odio autem quis minima expedita. Quam minima modi quisquam et similique rem incidunt, ducimus necessitatibus nesciunt quaerat, officia possimus sint odio commodi eaque assumenda laboriosam pariatur optio autem dolores rerum? Amet nobis non reprehenderit dolore omnis in inventore, officia nam esse quaerat aspernatur maiores alias tenetur commodi, sed voluptatem similique sit labore minus quasi delectus. Corrupti eligendi sequi eius vel consectetur, dolor repudiandae, atque, consequuntur explicabo minus quas doloribus tempore pariatur libero unde!',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum ketiga',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quisquam facere possimus nam molestiae, debitis quo vel quae exercitationem sed tempore voluptatibus, culpa quibusdam fugiat dolor commodi odio autem quis minima expedita. Quam minima modi quisquam et similique rem incidunt, ducimus necessitatibus nesciunt quaerat, officia possimus sint odio commodi eaque assumenda laboriosam pariatur optio autem dolores rerum? Amet nobis non reprehenderit dolore omnis in inventore, officia nam esse quaerat aspernatur maiores alias tenetur commodi, sed voluptatem similique sit labore minus quasi delectus. Corrupti eligendi sequi eius vel consectetur, dolor repudiandae, atque, consequuntur explicabo minus quas doloribus tempore pariatur libero unde!',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum keempat',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quisquam facere possimus nam molestiae, debitis quo vel quae exercitationem sed tempore voluptatibus, culpa quibusdam fugiat dolor commodi odio autem quis minima expedita. Quam minima modi quisquam et similique rem incidunt, ducimus necessitatibus nesciunt quaerat, officia possimus sint odio commodi eaque assumenda laboriosam pariatur optio autem dolores rerum? Amet nobis non reprehenderit dolore omnis in inventore, officia nam esse quaerat aspernatur maiores alias tenetur commodi, sed voluptatem similique sit labore minus quasi delectus. Corrupti eligendi sequi eius vel consectetur, dolor repudiandae, atque, consequuntur explicabo minus quas doloribus tempore pariatur libero unde!',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
+
     }
 }
--- belajar/routes/web.php
+++ belajar/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Models\Category;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Home Page
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+// About Page
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Azhari Arsyad",
+        "email" => "azhari.18des@gmail.com",
+        "image" => "azhari.jpeg"
+    ]);
+});
+
+// Posts Page
+Route::get('/posts', [PostController::class, 'index']);
+
+// Post Page
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+// Categories Page
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+// Category Page
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'title' => "Post By Category : $category->name",
+        'posts' => $category->posts,        
+    ]);
+});
+
+//Author Page
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('Posts', [
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->posts        
+    ]);
 });
