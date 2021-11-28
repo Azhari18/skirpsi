144517fdb67acc120c85c761d9ea450b018ab07b
Azhari18 <azhari.18des@gmail.com>
2021-11-28 19:47:13 +0700

    create post form

--- belajar/resources/views/dashboard/layouts/main.blade.php
+++ belajar/resources/views/dashboard/layouts/main.blade.php
@@ -10,11 +10,21 @@
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
+
+    {{-- Trix Editor --}}
+    <link rel="stylesheet" type="text/css" href="/css/trix.css">
+    <script type="text/javascript" src="/js/trix.js"></script>
+
+    <style>
+        trix-toolbar [data-trix-button-group='file-tools']{
+            display: none;
+        }
+    </style>
 </head>
 
 <body>
 
-@include('dashboard.layouts.header')
+    @include('dashboard.layouts.header')
     <div class="container-fluid">
         <div class="row">
 
